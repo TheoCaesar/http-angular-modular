26004274a9251e8cca5a0bd16894d4f21452d36e
TheoCaesar <theocaesar7@gmail.com>
2024-08-16 22:16:11 +0100

    Transform data with RxJS map operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -23,7 +24,18 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts() {
-    this.http.get(this.apiUrl+'posts.json').subscribe((data)=>
+    this.http.get(this.apiUrl+'posts.json')
+    .pipe(
+      map((data)=>{
+        const postArray = [];
+        for (const eachKey in data){
+          if (data.hasOwnProperty(eachKey))
+          postArray.push({...data[eachKey], id :eachKey})
+        }
+        return postArray;
+      })
+    )
+    .subscribe((data)=>
     console.log(data))
   }
 
