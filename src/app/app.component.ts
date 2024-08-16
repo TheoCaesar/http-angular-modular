f3b2491a35b99a3110e5df3a2545177bdaebd29d
TheoCaesar <theocaesar7@gmail.com>
2024-08-16 22:32:00 +0100

    Setting Types on HTTP Request Methods with Generics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+export interface Post {
+  id?:string;
+  title: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,18 +22,18 @@ export class AppComponent implements OnInit {
     this.onFetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post(this.apiUrl+"posts.json", postData).subscribe((data)=>{
+    this.http.post<Post>(this.apiUrl+"posts.json", postData).subscribe((data)=>{
       console.log(data)
     })
   }
 
   onFetchPosts() {
-    this.http.get(this.apiUrl+'posts.json')
+    this.http.get<{[key:string]: Post}>(this.apiUrl+'posts.json')
     .pipe(
-      map((data)=>{
-        const postArray = [];
+      map((data: {[key:string]: Post}):Post[]=>{
+        const postArray: Post[] = [];
         for (const eachKey in data){
           if (data.hasOwnProperty(eachKey))
           postArray.push({...data[eachKey], id :eachKey})
