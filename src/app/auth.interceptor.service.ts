e093476eddaef9f44d20a0833d4d030f920c76ec
TheoCaesar <theocaesar7@gmail.com>
2024-08-20 07:56:32 +0100

    multiple interceptors

--- src/app/auth.interceptor.service.ts
+++ src/app/auth.interceptor.service.ts
@@ -6,10 +6,11 @@ export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const dummyRequest = req.clone()
         dummyRequest.headers.append('custom-header', 'hi mother socker')
+        console.log(dummyRequest.headers)
         return next.handle(dummyRequest).pipe(
             tap((response)=>{
                 if (response.type === HttpEventType.Response) {
-                    console.dir(response.statusText)
+                    // console.dir(response.statusText)
                 }
             })
         )
